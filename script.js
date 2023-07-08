22ade52aeaf3d36d312e80b5e3ae176e2d7c276f
Silvio Cesar <silvio.cesarrx@hotmail.com>
2023-07-08 01:26:41 -0300

    Personal Customs

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ function toggleMode() {
   const html = document.documentElement
 
   console.log(html.classList)
-
   html.classList.toggle("light")
-
   const img = document.querySelector("#profile img")
-  console.log(img)
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
